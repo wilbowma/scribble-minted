7b317d2e54189641ec3c0aca7ce3c5bc7bbaec64
William J. Bowman <wjb@williamjbowman.com>
2019-02-15 22:00:01 -0800

    Added info file

---
+++ info.rkt
@@ -0,0 +1,7 @@
+#lang info
+(define collection 'multi)
+(define deps '("base"))
+(define build-deps '())
+(define pkg-desc "A package for typesetting code in Scribble via Pygmentize. Inspired by LaTeX's minted.")
+(define version "0.1")
+(define pkg-authors '(wilbowma))
