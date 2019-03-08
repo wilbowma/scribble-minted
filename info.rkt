10b31bf20f67a25db46d46edb0467bb2fe71aca1
William J. Bowman <wjb@williamjbowman.com>
2019-03-08 10:57:22 -0800

    Exclude minted.rkt from test
    It expects to be loaded from scribble/run.rkt, and loading from test has
    unexpected behavior.

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define deps '("rackunit-lib"
                "scribble-lib"
                "base"))
+(define test-omit-paths '("scribble/minted.rkt"))
 (define build-deps '())
 (define pkg-desc "A package for typesetting code in Scribble via Pygmentize. Inspired by LaTeX's minted.")
 (define version "0.1")
