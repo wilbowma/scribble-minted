23cfd9d8be60925d56407686b6bc5f5bf18a7eef
William J. Bowman <wjb@williamjbowman.com>
2021-03-29 16:37:54 -0700

    Remove inline-table display property for mintinline
    This doesn't seem to be required for inline, and screws up whitespace on some browsers

--- info.rkt
+++ info.rkt
@@ -6,5 +6,5 @@
 (define test-omit-paths '("scribble/minted.rkt"))
 (define build-deps '())
 (define pkg-desc "A package for typesetting code in Scribble via Pygmentize. Inspired by LaTeX's minted.")
-(define version "0.3")
+(define version "0.3.2")
 (define pkg-authors '(wilbowma))
